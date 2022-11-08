20862b6d1ef80e3a168f5a0c28da6d86fc5a4d81
Marvvllo <marvtfs@gmail.com>
2022-11-08 08:34:04 +0800

    Added firebase dependencies

---
+++ app/src/main/java/com/marvello/eleganter/FirebaseHelper.java
@@ -0,0 +1,14 @@
+package com.marvello.eleganter;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class FirebaseHelper {
+
+
+}
---
+++ app/src/main/java/com/marvello/eleganter/Furniture.java
@@ -0,0 +1,53 @@
+package com.marvello.eleganter;
+
+public class Furniture {
+    public String key, name, image, brand, specs;
+
+    public Furniture(String name, String image, String brand, String specs) {
+        this.name = name;
+        this.image = image;
+        this.brand = brand;
+        this.specs = specs;
+    }
+
+    public String getCode() {
+        return key;
+    }
+
+    public void setCode(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSpecs() {
+        return specs;
+    }
+
+    public void setSpecs(String specs) {
+        this.specs = specs;
+    }
+
+}
